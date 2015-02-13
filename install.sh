ba30ec16c25b4da4f105ef6bc404c6f14f006a85
Michał Budzyński <michal@virtualdesign.pl>
2015-02-13 19:24:14 +0300

    :speedboat: simple, basic install script added

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp OpenJSCAD.org/csg.js csg.js
+cp OpenJSCAD.org/openscad.js openscad.js
+cp OpenJSCAD.org/openjscad.js openjscad.js
+mkdir imgs
+cp OpenJSCAD.org/imgs/busy.gif imgs/busy.gif
